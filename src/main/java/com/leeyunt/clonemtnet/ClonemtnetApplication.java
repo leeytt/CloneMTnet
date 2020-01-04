d4d55cb9bb9c8767e8154c617949bbab4e0d81c0
leeyunt <924721970@qq.com>
2020-01-04 16:58:13 +0800

    Updata

--- src/main/java/com/leeyunt/clonemtnet/ClonemtnetApplication.java
+++ src/main/java/com/leeyunt/clonemtnet/ClonemtnetApplication.java
@@ -1,13 +1,11 @@
 package com.leeyunt.clonemtnet;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@EnableSwaggerBootstrapUI
 @SpringBootApplication
 @MapperScan("com.leeyunt.clonemtnet.dao")  //自动扫描路径下的所有mapper
 public class ClonemtnetApplication {
--- src/main/java/com/leeyunt/clonemtnet/config/Swagger2Config.java
+++ src/main/java/com/leeyunt/clonemtnet/config/Swagger2Config.java
@@ -1,9 +1,11 @@
 package com.leeyunt.clonemtnet.config;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,7 +24,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-@EnableSwaggerBootstrapUI
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 public class Swagger2Config {
     @Value("${swagger.title}")
     private String title;
