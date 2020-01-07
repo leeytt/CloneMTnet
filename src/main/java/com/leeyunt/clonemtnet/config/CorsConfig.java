ece47625fdfa44dbab7f29ebd71316fe182ca5c6
leeyunt <924721970@qq.com>
2020-01-07 16:11:49 +0800

    配置Cors解决跨域问题

---
+++ src/main/java/com/leeyunt/clonemtnet/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.leeyunt.clonemtnet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * Cors 解决跨域问题
+ *
+ * @author leeyunt
+ * @since 2020/01/07
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
